06c85a683c05659e15a721b04a1f6a19ec3a386a
Caleb Norris <paintballerz15@gmail.com>
2023-04-16 17:43:30 -0700

    Quote Route (Price Calculation)

--- server/routes/quotes/quote_routes.js
+++ server/routes/quotes/quote_routes.js
@@ -4,12 +4,83 @@ const mongoose = require("mongoose");
 
 const Quote = require("../../models/quotes/quote_schema")
 
+const PolishPrice = require("../../models/pricing/polish_schema");
+const ProtPrice = require("../../models/pricing/protection_schema");
+
 router.get('/', (req, res) => {
     res.send("We are on quotes")
 
 });
 
 router.post("/", (req, res) => {
+
+    let price = 0;
+    let polishPricing;
+    let protPricing;
+    let polishLevel = 0;
+    let size = "";
+
+
+    if (req.body.waterspots === true) polishLevel += 1;
+    if (req.body.swirls === true) polishLevel += 1;
+    if (req.body.scratches === true) polishLevel += 1;
+
+    ProtPrice.findOne().then((items) => protPricing = items);
+
+    
+    switch(req.body.carSize) {
+      case "smartCar":
+        size = "small";
+        break;
+      case "porsche":
+        size = "small";
+        break;
+      case "tesla":
+        size = "medium";
+        break;  
+      case "suv":
+        size = "large";
+        break;  
+      case "truck":
+        size = "large";
+        break;
+      case "van":
+        size = "xLarge"
+        break;    
+    }
+
+
+    PolishPrice.findOne({size: size}).then((items) => {
+      console.log(items);
+
+      switch(polishLevel) {
+        case 0:
+          price += items.enhancement;
+          break;
+        case 1:
+          price += items.oneStep;
+          break;
+        default:
+          price += items.twoStep;
+          break;   
+      }
+
+    });
+
+    console.log("Price: $" + price);
+
+    // console.log(polishPricing);
+  
+
+    if (protection.contains("wheels")) price += protPricing.wheels;
+    if (protection.contains("allWindows")) price += protPricing.allWindows;
+    if (protection.contains("windshield") && !protection.contains("allWindows")) price += protPricing.windshield;
+    if (protection.contains("paint")) price += protPricing.paint;
+    if (protection.contains("trimLights")) price += protPricing.trimLights;
+
+
+    
+  
     const newQuote = new Quote({
       name: req.body.name,
       email: req.body.email,
@@ -18,7 +89,7 @@ router.post("/", (req, res) => {
       swirls: req.body.swirls,
       scratches: req.body.scratches,
       protection: req.body.protection,
-      priceEstimation: req.body.priceEstimation,
+      priceEstimation: price,
       emailList: req.body.emailList
     });
 
